b2aa6b2debab080dde8470c491562854c19c1aca
SSen <i@ssen.name>
2019-08-31 09:55:41 +0900

    chore: @storybook/addon-storysource webpack config update

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,11 +3,28 @@ const {patchStorybookWebpackConfig} = require('react-zeroconfig');
 module.exports = async ({config}) => {
   patchStorybookWebpackConfig({config});
   
-  config.module.rules.push({
-    test: /\.stories\.(js|jsx|ts|tsx)$/,
-    loaders: [require.resolve('@storybook/addon-storysource/loader')],
-    enforce: 'pre',
-  });
+  config.module.rules.push(
+    {
+      test: /\.stories\.(js|jsx)$/,
+      loaders: [
+        {
+          loader: require.resolve('@storybook/addon-storysource/loader'),
+          options: {parser: 'javascript'},
+        },
+      ],
+      enforce: 'pre',
+    },
+    {
+      test: /\.stories\.(ts|tsx)$/,
+      loaders: [
+        {
+          loader: require.resolve('@storybook/addon-storysource/loader'),
+          options: {parser: 'typescript'},
+        },
+      ],
+      enforce: 'pre',
+    },
+  );
   
   return config;
 };
